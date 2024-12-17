91a9cffdfbcaf00d8f2cc24cc55fae84e5d00e56
Rushabh <rushabhbarot812@gmail.com>
2024-12-17 21:42:33 +0530

    initial project structure

---
+++ src/main/java/org/example/centralserver/CentralserverApplication.java
@@ -0,0 +1,13 @@
+package org.example.centralserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralserverApplication.class, args);
+	}
+
+}
