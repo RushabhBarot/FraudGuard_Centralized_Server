63c796ca029903a48bd687509b06630592b6f46d
Rushabh <rushabhbarot812@gmail.com>
2024-12-20 01:17:19 +0530

    added account service file

---
+++ src/main/java/org/example/centralserver/services/AccountService.java
@@ -0,0 +1,34 @@
+package org.example.centralserver.services;
+
+import org.example.centralserver.Entity.Account;
+import org.example.centralserver.repo.AccountRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.security.auth.login.AccountNotFoundException;
+
+@Service
+public class AccountService {
+
+    @Autowired
+    public AccountRepo accountRepo;
+
+    public AccountService(AccountRepo accountRepo) {
+        this.accountRepo = accountRepo;
+    }
+
+    public Account addaccount(Account account) {
+        return accountRepo.save(account);
+    }
+
+    public Account getaccount(String id) throws AccountNotFoundException {
+        return accountRepo.findByAccId(id)
+                .orElseThrow(() -> new AccountNotFoundException(id));
+    }
+
+    public Account getAccountByAccId(String accId) {
+        return accountRepo.findByAccId(accId).orElse(null);
+    }
+
+
+}
