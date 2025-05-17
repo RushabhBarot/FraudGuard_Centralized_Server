eb00b505129097c9501236ed9105e87e7f3f0221
Rushabh <rushabhbarot812@gmail.com>
2025-05-17 10:33:14 +0530

    updated transaction controller

--- src/main/java/org/example/centralserver/controller/TransactionController.java
+++ src/main/java/org/example/centralserver/controller/TransactionController.java
@@ -1,10 +1,19 @@
 package org.example.centralserver.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.example.centralserver.Entity.Transection;
+import org.example.centralserver.repo.mongo.AccountRepo;
+import org.example.centralserver.repo.mongo.TransectionRepo;
 import org.example.centralserver.services.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/transactions")
@@ -13,9 +22,35 @@ public class TransactionController {
     @Autowired
     private TransactionService transactionService;
 
+    @Autowired
+    private AccountRepo accountRepo;
+
+    @Autowired
+    private TransectionRepo transectionRepo;
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
     @GetMapping
-    public void fetchTransactions() throws InterruptedException {
+    public void fetchTransactions() throws Exception {
         System.out.println("Fetching transactions");
+        transectionRepo.deleteAll();
+        accountRepo.deleteAll();
+
+        // --- CLEAN Redis ---
+        Set<String> keys = redisTemplate.keys("*");
+        if (keys != null && !keys.isEmpty()) {
+            redisTemplate.delete(keys);
+            System.out.println("Deleted Redis keys: " + keys);
+        }
+
+
         transactionService.processTransactions();
+    }
+
+    @GetMapping("/getall")
+    public ResponseEntity<List<Transection>> getAllTransactions() throws JsonProcessingException {
+        List<Transection>transectionList=transectionRepo.findAll();
+        return ResponseEntity.ok(transectionList);
     }
 }
